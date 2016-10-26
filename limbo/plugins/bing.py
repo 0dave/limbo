c1d4c198bbebac9fb167c2b41e46ca2119aaf07e
0dave <davidiling@yahoo.com>
2016-10-26 12:54:43 -0400

    Update bing.py

--- limbo/plugins/bing.py
+++ limbo/plugins/bing.py
@@ -1,4 +1,4 @@
-"""!search <query> will return the randon bing image result for that query (!bing is an alias)"""
+"""!gif <search term> return a random result from the  google all gif search result for <search term>"""
 
 try:
     from urllib import quote
@@ -8,35 +8,33 @@ import re
 import requests
 from random import shuffle
 
-
 def unescape(url):
-    # for unclear reasons, bing replaces url escapes with \x escapes
+    # for unclear reasons, google replaces url escapes with \x escapes
     return url.replace(r"\x", "%")
 
-def bing(query):
-    query = quote(query)
-    
-    url = "https://www.bing.com/images/search?q={}&FORM=HDRSC2".format(query)
-    
-    # this is an old iphone user agent. Seems to make bing return good results.
+def gif(searchterm):
+    searchterm = quote(searchterm)
+
+    searchurl = "https://www.google.com/search?tbs=itp:animated&tbm=isch&q={0}{1}".format(searchterm)
+
+    # this is an old iphone user agent. Seems to make google return good results.
     useragent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Versio  n/4.0.5 Mobile/8A293 Safari/6531.22.7"
 
-    result = requests.get(url, headers={"User-agent": useragent}).text
+    result = requests.get(searchurl, headers={"User-agent": useragent}).text
 
-    bings = list(map(unescape, re.findall(r"var u='(.*?)'", result)))
-    shuffle(bings)
+    gifs = list(map(unescape, re.findall(r"var u='(.*?)'", result)))
+    shuffle(gifs)
 
-    if bings:
-        return bings[0]
+    if gifs:
+        return gifs[0]
     else:
         return ""
 
 def on_message(msg, server):
     text = msg.get("text", "")
-    match = re.findall(r"!(?:bing|search) (.*)", text)
+    match = re.findall(r"!ngif (.*)", text)
     if not match:
         return
 
-    
-    query = match[0]
-    return bing(query.encode("utf8"))
+    searchterm = match[0]
+    return gif(searchterm.encode("utf8"))
