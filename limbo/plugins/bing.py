d54e007dfc82e9a6a96dde7d86fd0dcdd3d35a91
0dave <davidiling@yahoo.com>
2016-10-26 13:17:05 -0400

    Update bing.py

--- limbo/plugins/bing.py
+++ limbo/plugins/bing.py
@@ -15,7 +15,7 @@ def unescape(url):
 def gif(searchterm):
     searchterm = quote(searchterm)
 
-    searchurl = "https://www.google.com/search?tbs=itp:animated&tbm=isch&q={0}{1}".format(searchterm)
+    searchurl = "https://www.google.com/search?tbs=itp:animated&tbm=isch&q={0}".format(searchterm)
 
     # this is an old iphone user agent. Seems to make google return good results.
     useragent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Versio  n/4.0.5 Mobile/8A293 Safari/6531.22.7"
