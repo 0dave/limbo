f7b6ab950b0881b2a7365d6d32e874933d4174ac
0dave <davidiling@yahoo.com>
2016-10-26 07:25:28 -0400

    Update image.py

--- limbo/plugins/image.py
+++ limbo/plugins/image.py
@@ -1,4 +1,4 @@
-"""!image <search term> return a random result from the google image search result for <search term>"""
+"""!image <search term> return a random result from the Bing image search result for <search term>"""
 
 try:
     from urllib import quote
@@ -16,7 +16,7 @@ def image(searchterm, unsafe=False):
     searchterm = quote(searchterm)
 
     safe = "&safe=" if unsafe else "&safe=active"
-    searchurl = "https://www.google.com/search?tbm=isch&q={0}{1}".format(searchterm, safe)
+    searchurl = "https://www.bing.com/search?tbm=isch&q={0}{1}".format(searchterm, safe)
 
     # this is an old iphone user agent. Seems to make google return good results.
     useragent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Versio  n/4.0.5 Mobile/8A293 Safari/6531.22.7"
